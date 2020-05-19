eac9897b49db1c4601b15e69c23943e5bae99093
Nextafari <chinedue16@gmail.com>
2020-05-19 16:08:46 +0100

    Feat::snake:login completed

--- source/my_login_page/settings.py
+++ source/my_login_page/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = "home/emmanuel/Desktop/schoolify_project/login/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
